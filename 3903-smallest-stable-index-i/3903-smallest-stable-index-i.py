623bb1634cf9713f45c5469f9ce42a21599b1b32
Jasmine Tripathy <jasminetrip123@gmail.com>
2026-09-04 11:50:41 +0530

    Time: 0 ms (100%), Space: 12.3 MB (96.3%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def firstStableIndex(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n= len(nums)
+        arr= [0] * n
+        arr[n-1]= nums[n-1]
+        for i in range(n-2, -1, -1):
+            arr[i]= min(arr[i+1], nums[i])
+        maxx= nums[0]
+        for i in range(n):
+            maxx= max(maxx, nums[i])
+            if maxx - arr[i]<= k:
+                return i
+        return -1
+
